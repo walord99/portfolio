a564b0acac139d09db40732ae75664ca2d8cb659
Benjamin Plante <benplante99@gmail.com>
2024-09-19 17:35:52 -0400

    base path added portfolio tes

--- src/projects/cube3d.tsx
+++ src/projects/cube3d.tsx
@@ -2,7 +2,7 @@ import { ProjectPreviewInfo } from "../components/ProjectPreview";
 
 const cube3d: ProjectPreviewInfo = {
 	name: "Cube3D",
-	logos_path: ["/c.svg"],
+	logos_path: ["/portfolio/c.svg"],
 	content_path: "src/projects/cube3d.md",
 	link: "https://github.com/walord99/cube3d",
 	language: "/c.svg",
