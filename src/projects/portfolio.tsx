dc0b8fa80fadab1b80792873c505ae2042889f49
Benjamin Plante <benplante99@gmail.com>
2024-10-01 17:16:44 -0400

    portfolio git link fix

--- src/projects/portfolio.tsx
+++ src/projects/portfolio.tsx
@@ -4,7 +4,7 @@ const portfolio: ProjectPreviewInfo = {
 	name: "Portfolio",
 	logos_path: ["react.svg", "css.svg", "three.svg"],
 	content_path: "project_mds/portfolio.md",
-	link: "https://github.com/walord99/minishell",
+	link: "https://github.com/walord99/portfolio",
 	language: "react.svg",
 };
 
